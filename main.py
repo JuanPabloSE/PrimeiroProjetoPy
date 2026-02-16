25b36cb961490169501fb3d42f1bd7ecebf48ef7
JuanPabloSE <juancontas@protonmail.com>
2026-02-15 22:22:09 -0300

    Commit

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-print("Verificação de nome")
+print("---- MÉDIA NOTA FINAL ----")
 print()
 
-nome = input("Qual o seu nome?")
-if nome == " Juan":
-    print("Bem-vindo" + nome)
-elif nome == "Maria":
-    print("Bem-vindo" + nome)
+nome = input("Digite seu nome: ")
+nota1 = float(input("Digite sua primeira nota: "))
+nota2 = float(input("Digite sua segunda nota: "))
+
+# Cálculo da média
+media = (nota1 + nota2) / 2
+
+print(f"\nMédia final: {media:.2f}")
+
+# Estrutura condicional
+if media >= 7.0:
+    print(f"Parabéns {nome}, você foi APROVADO!")
+elif media >= 5.0:
+    print(f"{nome}, você está na RECUPERAÇÃO!")
+else:
+    print(f"{nome}, você infelizmente foi REPROVADO!")
